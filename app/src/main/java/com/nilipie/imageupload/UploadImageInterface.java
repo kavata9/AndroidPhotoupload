fbb8ef12be0bd2e683bb1c0e8ed23d7e2a419110
Agnes Kavata Musyoka <kavatamusyoka24@gmail.com>
2019-07-29 10:02:12 +0300

    add changes

--- app/src/main/java/com/nilipie/imageupload/UploadImageInterface.java
+++ app/src/main/java/com/nilipie/imageupload/UploadImageInterface.java
@@ -2,6 +2,7 @@ package com.nilipie.imageupload;
 
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
+import retrofit2.Call;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
